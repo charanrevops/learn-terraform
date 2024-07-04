56a6f291c5a2b12313ac44aa6c254b92cce5749c
Charan Reddy <kothacharanreddy@gmail.com>
2024-07-04 17:28:59 +0200

    Learn Terraform

--- 02-variables/variable.tf
+++ 02-variables/variable.tf
@@ -5,3 +5,14 @@ variable "exam" {
 output "the-value-of-x-is----" {
   value = var.exam
 }
+
+#List variables
+
+variable "a" {
+  default=[
+  10,"hello",true]
+}
+
+output "the_print_of_alist_is" {
+  value = "var.a"
+}
