36131ce5c1eb6b5f82c6ccae5163182512b82977
Charan Reddy <kothacharanreddy@gmail.com>
2024-07-05 14:45:02 +0200

    Learn Terraform

--- 03-filters/main.tf
+++ 03-filters/main.tf
@@ -2,5 +2,5 @@ variable "fruits" {}
 
 
 output "apple_details_is" {
-  value = "apples quanity - ${var.fruits["quantity"]},Price is ${var.fruits["price"]}"
+  value = "apples quanity - ${var.fruits["apple"]["quantity"]},Price is ${var.fruits["apple"]["price"]}"
 }
