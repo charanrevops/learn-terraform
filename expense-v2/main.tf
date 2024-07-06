8a9c4f55e93fbeb8650ec9b1540230acedf8f7ed
Charan Reddy <kothacharanreddy@gmail.com>
2024-07-06 10:32:10 +0200

    Learn Terraform

---
+++ expense-v2/main.tf
@@ -0,0 +1,15 @@
+resource "aws_instance" "ec2-severnames" {
+  count         =  length(var.servernames)
+  ami           = "ami-041e2ea9402c46c32"
+  instance_type = "t3.small"
+  vpc_security_group_ids = ["sg-076b871708f2b2227"]
+
+  tags = {
+    Name = var.servernames
+  }
+}
+
+variable "servernames" {
+  defaukt = ["frontendserver", "backendserver", "mysqlserver"]
+
+}
