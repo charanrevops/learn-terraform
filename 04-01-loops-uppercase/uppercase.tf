6ab8f3983a75ffbd5c17a7b23d487a5187496401
Charan Reddy <kothacharanreddy@gmail.com>
2024-07-06 14:05:50 +0200

    Learn Terraform

--- 04-01-loops-uppercase/uppercase.tf
+++ 04-01-loops-uppercase/uppercase.tf
@@ -134,7 +134,7 @@ output "trainer-age" {
 }
 
 output "trainer-name" {
-  value = { for i,j in null_resource.users1 : "The Trainer name is ${i.triggers.trainername}" =>  "${i.triggers.coursename}"}
+  value = { for i in null_resource.users1 : "The Trainer name is ${i.triggers.trainername}" =>  "${i.triggers.coursename}"}
 }
 
 
