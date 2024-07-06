e9192936e0203e694f452396a933b72e845819bf
Charan Reddy <kothacharanreddy@gmail.com>
2024-07-06 12:41:24 +0200

    Learn Terraform

--- 04-01-loops-uppercase/uppercase.tf
+++ 04-01-loops-uppercase/uppercase.tf
@@ -12,28 +12,37 @@ variable"names" {
   description = "A sample list of names"
   type        = list(string)
   default     = ["apple", "banana", "orange", "pinacolada"]
+
 }
+
+  resource "null_resource" "users" {
+    for_each = toset(var.names)
+
+    triggers = {
+      name = each.value
+    }
+  }
 
 
 output "the-given-name-of-fruits-is----" {
-  value = [for item in var.names:"the given fruit names is ${var.names[0]}"]
+  value = [for item in var.names:"the given fruit names is" user.trigger.name]
 }
 
-provider "null" {}
-
-variable "user_names" {
-  type    = list(string)
-  default = ["user1", "user2", "user3"]
-}
-
-resource "null_resource" "users" {
-  for_each = toset(var.user_names)
-
-  triggers = {
-    name = each.value
-  }
-}
-
-output "user_names_output" {
-  value = [for user in null_resource.users : user.triggers.name]
-}
+# provider "null" {}
+#
+# variable "user_names" {
+#   type    = list(string)
+#   default = ["user1", "user2", "user3"]
+# }
+#
+# resource "null_resource" "users" {
+#   for_each = toset(var.user_names)
+#
+#   triggers = {
+#     name = each.value
+#   }
+# }
+#
+# output "user_names_output" {
+#   value = [for user in null_resource.users : user.triggers.name]
+# }
