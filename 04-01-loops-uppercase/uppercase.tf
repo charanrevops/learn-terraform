3ce0c4a1a8793434cef96ba86819a4c6d8804b0d
Charan Reddy <kothacharanreddy@gmail.com>
2024-07-06 12:30:28 +0200

    Learn Terraform

--- 04-01-loops-uppercase/uppercase.tf
+++ 04-01-loops-uppercase/uppercase.tf
@@ -12,9 +12,12 @@ variable"names"{
   description="A sample list of names"
   type=list(string)
   default = ["apple","banana","orange","pinacolada"]
+  tags = {
+    Name = var.names[count.index]
+  }
   #count=length(var.names)
 }
 
 output "the-given-name-of-fruits-is----" {
-  value = [for item in var.names:"the given fruit names is ${var.names[1]}"]
+  value = [for item in var.names:"the given fruit names is ${var.names}"]
 }
