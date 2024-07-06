b4aea58b7917346844b5cfb7e4da814388f3aaff
Charan Reddy <kothacharanreddy@gmail.com>
2024-07-06 12:47:00 +0200

    Learn Terraform

--- 04-01-loops-uppercase/uppercase.tf
+++ 04-01-loops-uppercase/uppercase.tf
@@ -36,5 +36,5 @@ resource "null_resource" "users" {
 }
 
 output "user_names_output" {
-  value = [for user in null_resource.users : user.triggers.name]
+  value = [for user in null_resource.users : "${user.triggers.name}"]
 }
