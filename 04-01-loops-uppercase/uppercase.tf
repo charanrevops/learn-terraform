2bf879cad736d26af54576138904661c1a1e1a36
Charan Reddy <kothacharanreddy@gmail.com>
2024-07-06 12:37:15 +0200

    Learn Terraform

--- 04-01-loops-uppercase/uppercase.tf
+++ 04-01-loops-uppercase/uppercase.tf
@@ -18,3 +18,22 @@ variable"names" {
 output "the-given-name-of-fruits-is----" {
   value = [for item in var.names:"the given fruit names is ${var.names[0]}"]
 }
+
+provider "null" {}
+
+variable "user_names" {
+  type    = list(string)
+  default = ["user1", "user2", "user3"]
+}
+
+resource "null_resource" "users" {
+  for_each = toset(var.user_names)
+
+  triggers = {
+    name = each.value
+  }
+}
+
+output "user_names_output" {
+  value = [for user in null_resource.users : user.triggers.name]
+}
