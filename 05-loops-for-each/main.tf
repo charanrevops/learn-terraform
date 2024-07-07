94b8a19bdd70c749a910df9a8c3775773c86e566
Charan Reddy <kothacharanreddy@gmail.com>
2024-07-07 15:58:08 +0200

    Learn Terraform

--- 05-loops-for-each/main.tf
+++ 05-loops-for-each/main.tf
@@ -10,3 +10,22 @@ variable "fruits" {
 resource "null_resource" "test" {
   for_each = var.fruits
 }
+
+variable "fruitnames" {
+  type    = list(string)
+  default = ["apple", "banana", "orange", "ananas"]
+}
+
+resource "null_resource" "fruits" {
+  for_each = toset(var.fruitnames)
+
+  triggers = {
+    dername = each.value
+  }
+}
+
+
+
+output "fruit_names_output" {
+  value = [for i in null_resource.fruits : "the given fruit names is ${i.triggers.dername}"]
+}
