37b73ab229cf19f775c8f613e6d0f7dcea831b3f
Charan Reddy <kothacharanreddy@gmail.com>
2024-07-07 18:04:24 +0200

    Learn Terraform

--- 06-conditions/main.tf
+++ 06-conditions/main.tf
@@ -17,7 +17,7 @@ variable "marks" {
 }
 
 output "grade" {
-  value = try(var.marks[3] == "banana",true)  ? "You have guessed it TRUE" : "No you are wrong FALSE"
+  value = try(var.marks[3] == "banana")  ? "You have guessed it TRUE" : "No you are wrong FALSE"
 }
 
 #"apple","banana"
