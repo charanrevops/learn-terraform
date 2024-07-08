aa5b96e4d9ea9f6df7ab847d7928b312d49136a5
Charan Reddy <kothacharanreddy@gmail.com>
2024-07-08 16:32:14 +0200

    Learn Terraform

--- expense-v4/main.tf
+++ expense-v4/main.tf
@@ -3,7 +3,7 @@ resource "aws_instance" "ec2-severnames" {
   for_each      = var.servernames
   ami           = "ami-041e2ea9402c46c32"
  # instance_type = try(each.value["instance_type"], null) == null? "t3.small" : each.value["instance_type"]
-  instance_type = lookup(var.servernames,each.value["instance_type"],"t3.small")
+  instance_type = lookup(var.servernames,each.value.instance_type,"t3.small")
 
   vpc_security_group_ids = ["sg-076b871708f2b2227"]
 
