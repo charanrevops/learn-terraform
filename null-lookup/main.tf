bddeba091e7c0d28f0a7e9cbfb40a6adcbf54e44
Charan Reddy <kothacharanreddy@gmail.com>
2024-07-08 18:18:54 +0200

    Learn Terraform

--- null-lookup/main.tf
+++ null-lookup/main.tf
@@ -4,12 +4,12 @@ resource "null_resource" "ec2-servernames" {
   # ami           = "ami-041e2ea9402c46c32"
   # instance_type = try(each.value["instance_type"], null) == null? "t3.small" : each.value["instance_type"]
   # instance_type = lookup(var.servernames[each.key],"instance_type","t3.small")
-  instance_type = lookup(each.value,"instance_type","t3.small")
+  # instance_type = lookup(each.value,"instance_type","t3.small")
   # vpc_security_group_ids = ["sg-076b871708f2b2227"]
 
-  tags = {
-    Name = each.key
-  }
+#   tags = {
+#     Name = each.key
+#   }
 }
 
 variable "servernames" {
