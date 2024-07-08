f9e4d7496b1ceb10f7696d7060f7e2869086df39
Charan Reddy <kothacharanreddy@gmail.com>
2024-07-08 18:27:33 +0200

    Learn Terraform

--- null-lookup/main.tf
+++ null-lookup/main.tf
@@ -28,7 +28,7 @@ variable "servernames" {
 
 
 output "names-ids" {
-  value = [for i in null_resource.ec2-servernames : "the given servername is ${var.servernames}"]
+  value = [for i in null_resource.ec2-servernames : "the given servername is ${var.servernames[0]}"]
 }
 
 
