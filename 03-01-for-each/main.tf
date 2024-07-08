a62f675826732c3a9d400bb6ae59b1b8aae3b05f
Charan Reddy <kothacharanreddy@gmail.com>
2024-07-08 16:23:47 +0200

    Learn Terraform

---
+++ 03-01-for-each/main.tf
@@ -0,0 +1,14 @@
+resource "null_resource" "ec2-severnames" {
+  count         =  length(var.servernames)
+  ami           = "ami-041e2ea9402c46c32"
+  instance_type = "t3.small"
+  vpc_security_group_ids = ["sg-076b871708f2b2227"]
+
+  tags = {
+    Name = var.servernames[count.index]
+  }
+}
+
+variable "servernames" {
+  default = ["frontendserver", "backendserver", "mysqlserver"]
+}
--- expense-v4/main.tf
+++ expense-v4/main.tf
@@ -2,7 +2,9 @@ resource "aws_instance" "ec2-severnames" {
   #count         =  length(var.servernames)
   for_each      = var.servernames
   ami           = "ami-041e2ea9402c46c32"
-  instance_type = try(each.value["instance_type"], null) == null? "t3.small" : each.value["instance_type"]
+ # instance_type = try(each.value["instance_type"], null) == null? "t3.small" : each.value["instance_type"]
+  instance_type = lookup(var.servernames,each.value,"t3.small")
+
   vpc_security_group_ids = ["sg-076b871708f2b2227"]
 
   tags = {
