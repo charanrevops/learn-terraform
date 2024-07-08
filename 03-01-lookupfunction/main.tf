4db9d8df752f14cff0c162f7eb21bf58e2c73313
Charan Reddy <kothacharanreddy@gmail.com>
2024-07-08 09:08:07 +0200

    Learn Terraform

--- 03-01-lookupfunction/main.tf
+++ 03-01-lookupfunction/main.tf
@@ -15,6 +15,6 @@ variable "fruits" {
 }
 output "the-fruit-name-is" {
   #value = lookup(var.fruits["name4"],"please say correct name")
-  value = lookup(var.fruits,string,"please say correct name")
+  value = lookup(var.fruits,name,"please say correct name")
 }
 
