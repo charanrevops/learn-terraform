a27293cf3ea80677b326f5c992fcf679429fd1f9
Charan Reddy <kothacharanreddy@gmail.com>
2024-07-12 15:13:52 +0200

    Learn Terraform

--- 07-module/sample-module/main.tf
+++ 07-module/sample-module/main.tf
@@ -1,7 +1,7 @@
 # this is the child module
 resource "null_resource" "example" {
   #count=length(var.fruits)
-  for_each = var.fruits[0]
+  for_each = [for v in null_resource.example: var.fruits]
 }
 
 variable  "fruits" {}
