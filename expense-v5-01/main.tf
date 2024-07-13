df5e0f949dc6e0bc07327da6130f3942f0626b3c
Charan Reddy <kothacharanreddy@gmail.com>
2024-07-13 14:46:36 +0200

    Learn Terraform

--- expense-v5-01/main.tf
+++ expense-v5-01/main.tf
@@ -4,5 +4,6 @@ module "expense-module" {
   instance_type     = each.value["instance_type"]
   name              = each.key
   vpc_security_group_ids = var.vpc_security_group_ids
+  for_each = var.servernames
 }
 
