33b8b16c2559810728859f78b263428fcb744432
Charan Reddy <kothacharanreddy@gmail.com>
2024-07-13 14:44:56 +0200

    Learn Terraform

--- expense-v5-01/childmodule/main.tf
+++ expense-v5-01/childmodule/main.tf
@@ -1,9 +1,9 @@
 resource "aws_instance" "ec2-servernames" {
 
-  ami           = "ami-041e2ea9402c46c32"
+  ami           = var.ami
 
   instance_type = var.instance_type
-  vpc_security_group_ids = ["sg-076b871708f2b2227"]
+  vpc_security_group_ids =var.vpc_security_group_ids
   tags = {
     Name = var.name
   }
--- expense-v5-01/childmodule/variables.tf
+++ expense-v5-01/childmodule/variables.tf
@@ -1,2 +1,4 @@
 variable "instance_type" {}
 variable "name" {}
+variable "vpc_security_group_ids" {}
+variable "ami" {}
