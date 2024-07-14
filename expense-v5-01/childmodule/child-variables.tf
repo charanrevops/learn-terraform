136d12ab76a8f471a416a7e19d6f0d3e6deb7089
Charan Reddy <kothacharanreddy@gmail.com>
2024-07-14 10:46:30 +0200

    Learn Terraform

--- expense-v5-01/childmodule/child-variables.tf
+++ expense-v5-01/childmodule/child-variables.tf
@@ -1,6 +1,4 @@
 variable "instance_type" {}
-variable "name" {
-  default = "rohitsharma"
-}
+variable "name" {}
 variable "vpc_security_group_ids" {}
 variable "ami" {}
--- expense-v5-01/main.tf
+++ expense-v5-01/main.tf
@@ -17,3 +17,28 @@ module "expense-module" {
 #     for_each = var.servernames
 #   }
 #
+
+
+variable "servernames" {
+  default = {
+    1-frontendserver = {
+
+    }
+    backendserver = {
+      #       instance_type = "t2.micro"
+    }
+    mysqlserver = {
+      #instance_type = "t2.micro"
+    }
+  }
+
+}
+
+
+variable "ami" {
+    default = "ami-041e2ea9402c46c32"
+}
+
+variable "vpc_security_group_ids" {
+    default = ["sg-076b871708f2b2227"]
+}
