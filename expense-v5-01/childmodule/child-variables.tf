bd2a012f175a2939392709d19f9a60e6f6906758
Charan Reddy <kothacharanreddy@gmail.com>
2024-07-14 10:54:51 +0200

    Learn Terraform

--- expense-v5-01/childmodule/child-variables.tf
+++ expense-v5-01/childmodule/child-variables.tf
@@ -1,5 +1,7 @@
 variable "instance_type" {}
 variable "name" {}
-variable "vpc_security_group_ids" {}
+variable "vpc_security_group_ids" {
+  default = ["sg-076b871708f2b2227-00"]
+}
 variable "ami" {}
 
